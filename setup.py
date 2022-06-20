15473833f70728021d71400875ddd3b53f0f8fdc
æriess <iseurie@gmail.com>
2022-06-20 03:57:31 -0400

    clean up setup

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="s4d",
+    author="kavorite",
+    license="MIT",
+    description="structured state spaces for dm-haiku",
+    find_packages={"": "src"},
+    version="0.2.0",
+    install_requires=["dm-haiku>=0.0.6", "einops~=0.4", "numpy~=1.22", "jax~=0.3"],
+)
